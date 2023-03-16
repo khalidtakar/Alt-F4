c91d5a362f63f3e377858766b5487dba85d2c94b
Stegi5656 <joelis.staugaitis@city.ac.uk>
2023-03-16 12:31:51 +0000

    starting project

---
+++ MainProject/src/Time.java
@@ -0,0 +1,14 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+public class Time {
+    public static String getTime(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+}
+
+
+// DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(dtf.format(now));
