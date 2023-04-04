f1dfc7b4bdf475608299efd10f068444c0806f40
khalidtakar <Khalid.Takar@city.ac.uk>
2023-04-04 14:03:06 +0100

    Started off on getCustomerByEmail of the CustomerSQLHelper class and completed one the first methods of the class.

--- MainProject/src/app/Sale/CustomerSQLHelper.java
+++ MainProject/src/app/Sale/CustomerSQLHelper.java
@@ -2,6 +2,8 @@ package app.Sale;
 
 import app.JDBC;
 
+import java.sql.SQLException;
+
 public class CustomerSQLHelper extends JDBC {
 
     public CustomerSQLHelper() {
@@ -15,7 +17,36 @@ public class CustomerSQLHelper extends JDBC {
      */
     public Customer getCustomerByEmail(String email){
         Customer customer = null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
 
+        String sql = "SELECT * FROM customers WHERE email = ?";
+
+        try {
+            // establish connection to database
+            connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2018g11"
+                    ,"in2018g11_d","qj3GNH0I");
+
+            // create SQL query to retrieve customer by email
+
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, email);
+
+            // execute query and retrieve results
+            resultSet = statement.executeQuery();
+
+            // check if customer was found
+            if(resultSet.next()){
+                // create new customer object and populate with data from result set
+                customer = new Customer(resultSet.getString("email"),resultSet.getString("name"));
+
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         return customer;
     }
 
