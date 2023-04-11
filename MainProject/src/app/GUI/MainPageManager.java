1328aa7a7018d6dc73d6f2a78b5eda57bf014528
Stegi5656 <joelis.staugaitis@city.ac.uk>
2023-04-11 12:00:43 +0100

    modified manager page to include add advisor

--- MainProject/src/app/GUI/MainPageManager.java
+++ MainProject/src/app/GUI/MainPageManager.java
@@ -1,10 +1,8 @@
 package app.GUI;
 
-import app.Account.Advisor;
 import app.Account.Employee;
 import app.Account.EmployeeSQLHelper;
 import app.Main;
-import app.Sale.Ticket;
 import app.Sale.TicketController;
 import app.System.System;
 import app.System.SystemController;
@@ -31,12 +29,9 @@ public class MainPageManager {
     private JTextField taxRate;
     private JLabel taxRateDisplay;
 
-    private JButton changeDiscountButton;
-    private JTextField discountRate;
-    private JTextField flexDiscountRate;
-    private JTextField flexDiscountAmount;
-    private JLabel discountRateDisplay;
-    private JLabel flexDiscountRateDisplay;
+    private JButton addAdvisorButton;
+    private JTextField advisorName;
+    private JTextField advisorEmail;
 
     private JTable advisorTable;
     private DefaultTableModel tableModel;
@@ -89,7 +84,7 @@ public class MainPageManager {
                 taxRateDisplay.setText("Tax rate: " + taxRate + "%");
             }
         });
-        changeDiscountButton.addActionListener(new ActionListener() {
+        addAdvisorButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 /*TODO change discount rate to discountRate
